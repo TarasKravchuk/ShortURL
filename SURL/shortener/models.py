e7316fc6345e29ead58858a679963bcd50ad69fa
taras <happycloud2012@gmail.com>
2019-05-03 20:19:34 +0300

    working server partially

---
+++ SURL/shortener/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class ShortURL(models.Model):
+    url = models.CharField(max_length=220, )
+    shortcode = models.CharField(max_length=15, unique=True)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__ (self):
+        return str(self.url)
+
+    def __unicode__(self):
+        return str(self.url)
