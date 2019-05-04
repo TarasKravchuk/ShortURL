ccb14030039b6f2a34e82d6dc876fdf13615b39c
taras <happycloud2012@gmail.com>
2019-05-04 20:28:19 +0300

    added first HTML, logic works correctly

--- SURL/SURL/urls.py
+++ SURL/SURL/urls.py
@@ -1,11 +1,10 @@
-from shortener.views import redirect_view, redirectCBView, _test_view
+from shortener.views import redirectCBView, HomeView
 from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^ololo123/', _test_view),
-    url(r'^(?P<shortcode>[\w-]{6,15})/$', redirect_view),
+    url(r'^$', HomeView.as_view()),
     url(r'^b/(?P<shortcode>[\w-]+){6,15}/$', redirectCBView.as_view()),
 ]
 
--- SURL/shortener/views.py
+++ SURL/shortener/views.py
@@ -3,18 +3,15 @@ from django.shortcuts import render, get_object_or_404
 from django.views import View
 from .models import ShortURL
 
-def redirect_view(request, shortcode=None, *args, **kwargs):
-    obj = get_object_or_404(ShortURL, shortcode=shortcode)
-    return HttpResponseRedirect(obj.url)
-
 class redirectCBView(View):
     def get(self, request, shortcode=None, *args, **kwargs):
         obj = get_object_or_404(ShortURL, shortcode=shortcode)
         return HttpResponseRedirect(obj.url)
 
+
+
+class HomeView(View):
+    def get (self, request, *args, **kwargs):
+        return render(request, 'shortener/home.html', {})
     def post (self, request, *args, **kwargs):
-        return HttpResponse()
-
-
-def _test_view(request):
-    return HttpResponse("test_view")
+        return render(request, "shortener/home.html", {})
